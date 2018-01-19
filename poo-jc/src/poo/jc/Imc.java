7775a56c8d0abceb55849c7ddf92104c03878bd0
Juan Carlos Campos <rapidclimate@gmail.com>
2018-01-18 19:47:41 -0600

    segund clase

---
+++ poo-jc/src/poo/jc/Imc.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.jc;
+
+/**
+ *
+ * @author rapid
+ */
+public class Imc {
+    //El siguiente atribuo es de tipo
+    //composicion agregacion
+    Usuario u;
+    
+    public String calcular(){
+        
+        float imc= u.peso/(u.altura*u.altura);
+        
+        return "Tu resultado es "+imc;
+    }
+    
+    
+}
